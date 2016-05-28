30eff3455e6808fcc174b9f358b949d056080d6d
Les De Ridder <les@lesderid.net>
2016-05-28 22:43:44 +0200

    Add Paste and Deletion models

--- app/Deletion.php
+++ app/Deletion.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Deletion extends Model
 {
-    //
+	public $timestamps = false;
+
+	public function paste()
+	{
+		return $this->belongsTo(Paste::class);
+	}
 }
--- app/Paste.php
+++ app/Paste.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paste extends Model
 {
-    //
+	public $incrementing = false;
+	public $timestamps = false;
+
+	public function deletion()
+	{
+		return $this->hasOne(Deletion::class);
+	}
+
+	public function isDeleted()
+	{
+		return $this->deletion !== null;
+	}
+
+	public function delete($reason, $deleted_by)
+	{
+		if($this->isDeleted())
+		{
+			return false;
+		}
+
+		$deletion = new Deletion;
+		$deletion->reason = $reason;
+		$deletion->deleted_by = $deleted_by;
+		$deletion->deleted_at = Carbon::now();
+
+		$this->deletion()->save($deletion);
+
+		return true;
+	}
 }
